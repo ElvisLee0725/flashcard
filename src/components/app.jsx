847b389b20c4d6c95927436ee0783c359f69f2a1
ElvisLee <elvislee0725@gmail.com>
2020-02-24 19:54:16 -0800

    Setting up react flash card app

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-center">Flash Card App</h1>
+        </div>
+    );
+}
+
+export default App;
+
