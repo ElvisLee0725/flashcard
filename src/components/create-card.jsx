9fb943dd08765e86da1f93c267a0da8e9ed23cea
ElvisLee <elvislee0725@gmail.com>
2020-02-25 17:26:26 -0800

    Make CreateCard component functional, added context

--- src/components/create-card.jsx
+++ src/components/create-card.jsx
@@ -1,15 +1,68 @@
 import React from 'react';
+import { AppContext } from '../appContext';
 
 class CreateCard extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            question: '',
+            answer: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    handleChange({ target: {name, value }}) {
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const { question, answer } = this.state;
+        const newCard = {
+            question,
+            answer
+        }
+        this.context.addCard(newCard);
+        this.reset();
+    }
+
+    reset() {
+        this.setState({
+            question: '',
+            answer: ''
+        });
+        this.context.setView('view-cards');
     }
 
     render() {
+        const { question, answer } = this.state;
         return (
-            <h1 className="text-center">Create New Card</h1>
+            <div className="container">
+                <h1 className="text-center">Create New Card</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="question">Question:</label>
+                        <textarea type="text" className="form-control" value={question} name="question" onChange={this.handleChange} autoFocus></textarea>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="answer">Answer:</label>
+                        <textarea type="text" className="form-control" value={answer} name="answer" onChange={this.handleChange}></textarea>
+                    </div>
+                    
+                    <div className="form-group text-right">
+                        <button type="button" className="btn btn-danger mr-2" onClick={this.reset}>Cancel</button>
+                        <button type="submit" className="btn btn-primary">Save Card</button>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
+
+CreateCard.contextType = AppContext;
 
 export default CreateCard;
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
+import { AppContext } from '../appContext';
 
-function Nav(props) {
-    const { curView } = props;
-    console.log(curView);
-    const viewActive = curView === 'view-cards' ? 'active' : ''; 
-    const reviewActive = curView === 'review-cards' ? 'active' : ''; 
-    const createActive = curView === 'create-card' ? 'active' : ''; 
+class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.showComponent = this.showComponent.bind(this);
+    }
 
-    return (
-        <div className="nav-container">
-            <nav className="nav nav-pills nav-justified">
-                <a className={`nav-item nav-link ${viewActive}`} onClick={props.setView} id="view-cards">View Cards</a>
-                <a className={`nav-item nav-link ${reviewActive}`} onClick={props.setView} id="review-cards">Review</a>
-                <a className={`nav-item nav-link ${createActive}`} onClick={props.setView} id="create-card">Create Card</a>
-            </nav>
-        </div>
-    );
+    showComponent(e) {
+        this.context.setView(e.target.id);
+    }
+
+    render() {
+        const curView = this.context.view;
+
+        const viewActive = curView === 'view-cards' ? 'active' : ''; 
+        const reviewActive = curView === 'review-cards' ? 'active' : ''; 
+        const createActive = curView === 'create-card' ? 'active' : ''; 
+
+        return (
+            <div className="nav-container">
+                <nav className="nav nav-pills nav-justified">
+                    <a className={`nav-item nav-link ${viewActive}`} onClick={this.showComponent} id="view-cards">View Cards</a>
+                    <a className={`nav-item nav-link ${reviewActive}`} onClick={this.showComponent} id="review-cards">Review</a>
+                    <a className={`nav-item nav-link ${createActive}`} onClick={this.showComponent} id="create-card">Create Card</a>
+                </nav>
+            </div>
+        );
+    }
 }
+
+Nav.contextType = AppContext;
 
 export default Nav;
