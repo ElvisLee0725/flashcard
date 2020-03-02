61bb4eb2c6aa6b35e134e5887416a6034d589c1e
ElvisLee <elvislee0725@gmail.com>
2020-03-01 17:47:39 -0800

    Add modal component and implement delete function

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,20 +1,43 @@
 import React from 'react';
+import { AppContext } from '../appContext';
+import Modal from './modal';
 
-function Card(props) {
-    return (
-        <div className="col mb-4">
-            <div className="card h-100">
-                <div className="card-body card-question-bg">
-                    <h5 className="card-title text-muted">Question:</h5>
-                    <p className="card-text">{props.question}</p>
-                </div>
-                <div className="card-body card-answer-bg">
-                    <h5 className="card-title text-muted">Answer:</h5>
-                    <p className="card-text">{props.answer}</p>
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    // Set the activeCard prop as the card selected to delete
+    handleDelete() {
+        const { cards, setActiveCard } = this.context;
+        const indexToDelete = cards.findIndex((card) => {
+            return card.id === this.props.id;
+        });
+        setActiveCard(indexToDelete);
+    }
+
+    render() {
+        return (
+            <div className="col mb-4">
+                <div className="card h-100">
+                    <div className="card-body card-question-bg">
+                        <h5 className="card-title text-muted">Question:</h5>
+                        <p className="card-text">{this.props.question}</p>
+                    </div>
+                    <div className="card-body card-answer-bg">
+                        <h5 className="card-title text-muted">Answer:</h5>
+                        <p className="card-text">{this.props.answer}</p>
+                    </div>
+                    <div className="card-footer card-footer-bg text-center">
+                        <Modal handleDelete={this.handleDelete}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+Card.contextType = AppContext;
 
 export default Card;
---
+++ src/components/modal.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { AppContext } from '../appContext';
+
+class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        this.confirmRemove = this.confirmRemove.bind(this);
+    }
+
+    openModal() {
+        this.props.handleDelete();
+        this.setState({
+            isOpen: true
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            isOpen: false
+        });
+    }
+
+    confirmRemove() {
+        this.closeModal();
+        this.context.removeCard();
+    }
+
+    render() {
+        const { activeCard } = this.context;
+        if(this.state.isOpen) {
+            return (
+                <div className="basic-modal" onClick={this.closeModal}>
+                    <div onClick={(e) => e.stopPropagation()} className="basic-modal-content">
+                        <div onClick={this.closeModal} className="modal-close-btn">X</div>
+                        <h5 className="delete-title mb-4">Are you sure you want to delete this card?</h5>
+                        <p className="mb-2">{`Q: ${activeCard.question}`}</p>
+                        <p className="mb-2">{`A: ${activeCard.answer}`}</p>
+                        <div className="text-right mt-4">
+                            <button type="button" className="btn btn-outline-secondary mr-2" onClick={this.closeModal}>Cancel</button>
+                            <button type="button" className="btn btn-danger" onClick={this.confirmRemove}>Confirm</button>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
+        return (
+            <i className="fa fa-trash delete-btn" onClick={this.openModal}></i>
+        );
+    }
+}
+
+Modal.contextType = AppContext;
+
+export default Modal;
