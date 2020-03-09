ec4d64e5e1f05e7ca04c2414f819d5cbbd70f89e
ElvisLee <elvislee0725@gmail.com>
2020-03-08 21:12:40 -0700

    Implement card update function

--- src/components/update-card.jsx
+++ src/components/update-card.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { AppContext } from '../appContext';
+import CardForm from './card-form';
 
-class UpdateCard extends React.Component {
-    constructor(props){
-        super(props);
-    }
-
-    render() {
-        const { activeCard } = this.context;
-        return (
-            <div className="container">
-                <h1 className="text-center">Update Card</h1>
-                <p>{activeCard.question}</p>
-                <p>{activeCard.answer}</p>
-            </div>
-        );
-    }
+function UpdateCard() {
+    return (
+        <CardForm />
+    );
 }
-
-UpdateCard.contextType = AppContext;
 
 export default UpdateCard;
